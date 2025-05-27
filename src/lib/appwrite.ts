746d2f9a191a86105e5d9a732fd76506e2cbf6f1
charukirti <charukirticc112002@gmail.com>
2025-05-27 23:33:51 +0530

    feat: appwrite client setup

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,20 @@
+import { Account, Client, Databases, Storage } from "appwrite";
+
+const client = new Client();
+client
+  .setEndpoint(import.meta.env.VITE_APPWRITE_URL)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+
+export const databases = new Databases(client);
+export const account = new Account(client);
+export const storage = new Storage(client);
+
+export default client;
+
+export const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+
+export const COLLECTIONS = {
+  BLOGS: import.meta.env.VITE_APPWRITE_POSTS_COLLECTION_ID,
+  COMMENTS: import.meta.env.VITE_APPWRITE_COMMENTS_COLLECTION_ID,
+  LIKES: import.meta.env.VITE_APPWRITE_LIKES_COLLECTION_ID,
+};
